f4fdf13737623f57ed7d31aadf0723980eff8c89
taotruong <tt10052006@gmail.com>
2025-11-06 20:03:27 +0700

    gộp nhánh ktrasongto2 vào main

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 #include <iostream> 
 using namespace std;
+bool checkprime(int n)
+{
+    if (n < 2 ) return false;
+    for (int i = 2 ; i*i <=n; ++i)
+    {
+        if (n % i == 0) 
+            return false;
+    }
+    return true;
+}
 int main()
 {
     cout  << "Hello" << endl;
